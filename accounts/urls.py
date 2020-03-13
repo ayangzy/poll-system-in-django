cbd6f01bc4c1c0c18bd79bfd5ea281078990352f
Ayange Felix <ayangefelix8@gmail.com>
2020-03-13 15:16:34 -0700

    template and set up done

---
+++ accounts/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path, include
+from . import views
+
+
+urlpatterns = [
+    path('signup', views.register, name='register'),
+    path('login', views.login, name='login'),
+    
+]
---
+++ pages/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def index(request):
+    return render(request, 'pages/index.html')
+
+
+def contestants(request):
+    return render(request, 'pages/contestants.html')
