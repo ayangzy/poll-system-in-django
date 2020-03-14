d947eede7f557cfd17c47c3e2860403bf53bb0b7
Ayange Felix <ayangefelix8@gmail.com>
2020-03-14 15:44:58 -0700

    contestants model added

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Contestant
 
 # Register your models here.
+
+
+class ContestantAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'school', 'votes_total')
+    list_display_links = ('id', 'name')
+    search_fields = ('name',)
+    list_per_page = 25
+
+
+admin.site.register(Contestant, ContestantAdmin)
--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Contestant(models.Model):
+    name = models.CharField(max_length=200)
+    school = models.CharField(max_length=250)
+    image = models.ImageField(upload_to='images/%Y/%m/%d/')
+    votes_total = models.IntegerField(default=0)
+
+
+def __str__(self):
+    return self.name
