9cdd57eda37344e6886cfe789457e88f542341b6
Ayange Felix <ayangefelix8@gmail.com>
2020-03-15 06:37:37 -0700

    poll vote for contestants added

--- pages/urls.py
+++ pages/urls.py
@@ -1,9 +1,10 @@
 
-from django.urls import path, include
+from django.urls import path
 from . import views
 
 
 urlpatterns = [
-    path('', views.index, name='index'),  
-    path('contestants', views.contestants, name='contestants')
+    path('', views.index, name='index'),
+    path('contestants', views.contestants, name='contestants'),
+    path('<int:contestant_id>/vote', views.vote, name='vote')
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,11 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.contrib import auth
+from .models import Contestant
+
 
 # Create your views here.
 
 
 def index(request):
+
     return render(request, 'pages/index.html')
 
 
+@login_required(login_url='accounts/login')
 def contestants(request):
-    return render(request, 'pages/contestants.html')
+    cons = Contestant.objects.all
+    return render(request, 'pages/contestants.html', {'cons': cons})
+
+
+@login_required(login_url='accounts/login')
+def vote(request, contestant_id):
+    if request.method == 'POST':
+        cons = get_object_or_404(Contestant, pk=contestant_id)
+        cons.votes_total += 1
+        cons.save()
+        auth.logout(request)
+        return redirect('index')
